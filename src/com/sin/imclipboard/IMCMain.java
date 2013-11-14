f0988a160a722d41fb3685ec855c5a8fef5d9871
trb <sintrb@gmail.com>
2013-11-14 10:41:08 +0800

    Add version info for 1.2

--- src/com/sin/imclipboard/IMCMain.java
+++ src/com/sin/imclipboard/IMCMain.java
@@ -8,10 +8,18 @@ package com.sin.imclipboard;
  */
 public class IMCMain {
 
-	// version
-	public String VERSION = "1.1";
+	/**
+	 * Version of this application
+	 * 
+	 * @1.2 Add auto resize 2013.11.14
+	 * 
+	 * @V1.1 First version 2013.11.05
+	 */
+	public static String VERSION = "1.2";
+	public static String DATE = "2013.11.14";
 
 	public static void main(String[] args) {
+		System.out.println("IMClipboard V" + VERSION + " " + DATE);
 		new MainFrame().setVisible(true);
 	}
 }
